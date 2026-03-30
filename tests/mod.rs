3321f043b584c240b63739007a460638450a812a
Tony Arcieri <bascule@gmail.com>
2026-03-30 10:24:00 -0600

    Add tests for `Array::cast_slice_to_core(_mut)` (#187)

--- tests/mod.rs
+++ tests/mod.rs
@@ -27,6 +27,24 @@ fn cast_slice_from_core_mut() {
 }
 
 #[test]
+fn cast_slice_to_core() {
+    type A = Array<u8, U2>;
+    let arr = [Array([1, 2]), Array([3, 4])];
+    let slice = A::cast_slice_to_core(&arr);
+    assert_eq!(slice[0], [1, 2]);
+    assert_eq!(slice[1], [3, 4]);
+}
+
+#[test]
+fn cast_slice_to_core_mut() {
+    type A = Array<u8, U2>;
+    let mut arr = [Array([1, 2]), Array([3, 4])];
+    let slice = A::cast_slice_to_core_mut(&mut arr);
+    assert_eq!(slice[0], [1, 2]);
+    assert_eq!(slice[1], [3, 4]);
+}
+
+#[test]
 fn tryfrom_slice_for_clonable_array() {
     assert!(Array::<u8, U0>::try_from(EXAMPLE_SLICE).is_err());
     assert!(Array::<u8, U3>::try_from(EXAMPLE_SLICE).is_err());
